aa29d484e262c847ed3adcbe818268668491570b
László Péntek <pentek.laszlo1994@gmail.com>
2022-08-08 13:05:46 +0200

    0.6

---
+++ card.js
@@ -0,0 +1,28 @@
+fetch("card.json")
+.then(function(response){
+   return response.json();
+})
+.then(function(cards){
+   let placeholder = document.querySelector(`#card-output`);
+   let out = "";
+   for(let card of cards){
+      out += `
+      <section >
+      <div class="bekezdes">
+         <div class="card">
+            <img class="card-img" src="${card.kép}" alt="Krubi">
+            <div class="card-cim">${card.felépő}</div>     
+         </div>
+         <div class="icons">
+            <a href="${card.youtube}"><i class="fa-brands fa-square-facebook "></i></a>
+            <a href="instagram.com"><i class="fa-brands fa-square-instagram instagram"></i></a>
+            <a href="youtube.com"><i class="fa-brands fa-youtube"></i></a>
+         </div>
+      </div>
+      </section>
+ 
+      `;
+   }
+   placeholder.innerHTML = out;
+});
+
